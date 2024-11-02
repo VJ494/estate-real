531543e82afa28f10bfcb9c7c0b68584531af195
Victoria <vj808454@gamil.com>
2024-11-02 06:47:32 +0200

    signup

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,21 @@
 import User from "../models/user.model.js";
 import bcryptj from 'bcryptjs';
 
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-export const signup = async (req, res, next) =>{
-    const { username, email, password } =req.body;
+  // Check if all required fields are provided
+  if (!username || !email || !password) {
+    return res.status(400).json({ success: false, message: 'All fields are required' });
+  }
+
+  try {
     const hashedPassword = bcryptj.hashSync(password, 10);
-    const newUser = new User({ username, email, password:hashedPassword });
-    try {
-        await newUser.save()
-        res.status(201).json('User created successfully!')
-    } catch (error) {
-        next(error);
-    }
-    
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json('User created successfully!');
+  } catch (error) {
+    next(error);
+  }
 };
 
